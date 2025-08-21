5737d86a2c7e639275abbd030cd4b466426dfbab
satyayanamadala <satyayanamadala99@gmail.com>
2025-08-22 00:45:49 +0530

    variables commit

---
+++ VARIABLES/typecasting.java
@@ -0,0 +1,10 @@
+public class typecasting {
+    public static void main(String [] arg){
+        byte b = 127;
+        int a = b;
+        double c = 300.56;
+        int d = (int) c;
+        System.out.println(a);
+        System.out.println(d);
+    }
+}
---
+++ VARIABLES/variable.java
@@ -0,0 +1,17 @@
+public class variable {
+    public static void main(String [] arg){
+        String name = "satya";
+        System.out.println(3+3);
+        int num1 = 2;
+        int num2 = 3;
+        int num = num1 + num2;
+        float percentage = 8.69f;
+        char letter = 'S';
+        boolean satya = true;
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(percentage);
+        System.out.println(letter);
+        System.out.println(satya);
+    }
+}
